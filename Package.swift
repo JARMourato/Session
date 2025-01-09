f16a12b717a7d0a6cc47f2efac49d626ecd8ccf1
JARMourato <joao.armourato@gmail.com>
2025-01-09 22:34:44 +0000

    rollback change for macos15 in tests because CI is in 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Session",
-    platforms: [.iOS(.v18), .macOS(.v15), .watchOS(.v11), .tvOS(.v18)],
+    platforms: [.iOS(.v18), .macOS(.v14), .watchOS(.v11), .tvOS(.v18)],
     products: [
         .library(name: "Session", targets: ["Session"]),
     ],
